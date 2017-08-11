7d9d97c27f250ac0f1959186721139f7f5a4344c
Joris <joris.coppieters@gmail.com>
2017-08-11 16:43:29 +1200

    Set version to 0.1.24

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // ******************************
 //
 //
-// SERVICE PATH v0.1.23
+// SERVICE PATH v0.1.24
 //
 // 0.1.9
 // - Allow unsecured HTTPS requests
@@ -29,7 +29,7 @@ let paths = require('./src/paths');
 // Constants:
 // ******************************
 
-const k_VERSION = '0.1.23';
+const k_VERSION = '0.1.24';
 
 // ******************************
 // Functions:
